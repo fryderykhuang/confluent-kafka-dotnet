0bd499dff655b7d905a8baaf72e2e3193d6e9d91
Fryderyk Huang <fryderyk.huang@gmail.com>
2018-09-25 19:48:12 +0800

    filter out properties starts with 'dotnet.'

--- src/Confluent.Kafka/Producer.cs
+++ src/Confluent.Kafka/Producer.cs
@@ -999,8 +999,12 @@ namespace Confluent.Kafka
 
             var configHandle = SafeConfigHandle.Create();
 
-            modifiedConfig.ToList().ForEach((kvp) => {
-                if (kvp.Value == null) throw new ArgumentException($"'{kvp.Key}' configuration parameter must not be null.");
+            modifiedConfig.ToList().ForEach((kvp) =>
+            {
+                if (kvp.Value == null)
+                    throw new ArgumentException($"'{kvp.Key}' configuration parameter must not be null.");
+                if (kvp.Key.StartsWith("dotnet."))
+                    return;
                 configHandle.Set(kvp.Key, kvp.Value.ToString());
             });
 
